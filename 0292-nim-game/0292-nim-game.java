23c4ac5542139489b590be885af0203315c3f4bf
PRIYANSHU NAGARIYA <110557064+priyanshunagariya16@users.noreply.github.com>
2023-10-16 22:26:54 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (30.05%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canWinNim(int n) {
+         boolean k=true;
+        if(n<4)
+        k=true;
+        if(n>3)
+        {
+            if(n%4==0)
+            k=false;
+            else
+            k=true;
+        }
+        return k;
+    }
+}
